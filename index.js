79eefa570338cf44f1e3eb93b9a3a8ff69eba2b5
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-06-13 17:50:45 +0200

    Add toString method

---
+++ index.js
@@ -0,0 +1,11 @@
+import { LinkedList } from "./linked-list.js";
+
+const list = new LinkedList();
+
+list.append("terrazzo");
+list.append("sgabello");
+list.append("formaggio");
+list.append("borraccia blu");
+list.append("cannuccia");
+
+console.log(list.toString());
